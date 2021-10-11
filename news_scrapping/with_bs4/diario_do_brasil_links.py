d46732c186f9c9b0f1fa96513265af27cdfeaaa0
jpvasquesc <jpvasquesc@gmail.com>
2021-10-11 16:24:54 -0300

    diariodobrasil.org scrapping

---
+++ news_scrapping/with_bs4/diario_do_brasil_links.py
@@ -0,0 +1,88 @@
+"""
+Get the links for new posts listed in the page "https://diariodobrasil.org/todas-materias/".
+This page uses a AJAX requests for infinte scrolling.
+"""
+
+import argparse
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+def parse_input_arguments():
+    """
+    Create a command-line menu for this script using 'argparse'.
+    """
+    parser = argparse.ArgumentParser(prog="'Diario do Brasil' posts scrapping",
+                                     description="Scrape the webpage 'diariodobrasil.org/todas-materias/' for links of the most recent posts")
+    
+    parser.add_argument('security_token', type=str, help="Connection security token that updates daily.\
+                                                          How to find this token:\n\
+                                                          1 - While in the page 'https://diariodobrasil.org/todas-materias/', open your web browser's Dev Tools.\n\
+                                                          2 - Go to the 'Network' tab.\n\
+                                                          3 - Click the 'ver mais...' button at the bottom of the webpage.\n\
+                                                          4 - A process named 'admin-ajax.php' should appear, click on it.\n\
+                                                          5 - In the 'Headers' tab, find the 'Form Data' section.\n\
+                                                          6 - Under 'Form Data', find 'nonce:', the value at it's side is the security token.\n\
+                                                          Token example: '5b355af4ca'")
+
+    parser.add_argument('-p','--pages', dest='p', type=int, default=5, help="Number of pages to scrape")
+    return parser.parse_args()
+
+def get_article_listing_page(page: int, security_token: str):
+    """
+    Get the HTML for the post listing webpage.
+    """
+    
+    form_data = {'action': 'load_more',
+                    "class": "Essential_Addons_Elementor\Elements\Post_Grid",
+                    "args": "orderby=date&order=desc&ignore_sticky_posts=1&post_status=publish&posts_per_page=32&offset=15&post_type=post",
+                    "page": str(page),
+                    "page_id": "245827",
+                    "widget_id": "ebb0ef2", 
+                    "nonce": security_token, # This value seems to updates daily
+                    "template_info[dir]": "lite",
+                    "template_info[file_name]": "default.php",
+                    "template_info[name]": "Post-Grid"}
+    page_request = requests.post("https://diariodobrasil.org/wp-admin/admin-ajax.php", data=form_data)
+    response = page_request.json()
+    
+    if type(response) == str:
+        html = BeautifulSoup(response, features="lxml")
+        return html
+    
+    else:
+        raise ValueError("Wrong security token. Please check if when you click the 'ver mais...' button, more posts are loaded or not.")
+        
+def get_links(html):
+    """
+    Parse post's links out of html and save in a '.txt' file.
+    """
+    # Create file
+    txt_path = Path("../../news_scrapping/links/diario_do_brasil.txt")
+    txt_path.touch(exist_ok=True)
+
+    # Parse and save
+    with open(txt_path, "a") as txt_file:
+        links_list = [a_tag['href'] for a_tag in html.find_all('a', {"class": "eael-grid-post-link"}, href=True)]
+        for link in links_list:
+            txt_file.write("{0}\n".format(link))
+    
+    return len(links_list)
+
+def main():
+    """
+    Get the links for posts listed in the first 'PAGES' pages from the webpage "https://diariodobrasil.org/todas-materias/".
+    """
+    
+    args = parse_input_arguments()
+    
+    num_links = 0
+    for page in range(1, args.p):
+        html = get_article_listing_page(page, args.security_token)
+        num_links += get_links(html)
+        print("Parsed page {0}".format(page))
+    
+    print("Done! Saved {0} links from the page 'https://diariodobrasil.org/todas-materias/'.".format(num_links))
+
+if __name__ == "__main__":
+    main()
---
+++ news_scrapping/with_scrapy/with_scrapy/spiders/diario_do_brasil_articles.py
@@ -0,0 +1,44 @@
+import scrapy
+from pathlib import Path
+import re
+
+class DiarioDoBrasilArticlesSpider(scrapy.Spider):
+    """
+    Web scrapping 'spider', based on scrapy's 'Spider' class. 
+    Built to scrap the text of news articles under the 'diariodobrasil.org' domain.   
+    """
+    name = 'diario_do_brasil_articles'
+    allowed_domains = ['diariodobrasil.org']
+
+    # Get links to the news articles from which the text will be scrapped
+    links_path = Path("../../../links/diario_do_brasil.txt")
+    links_path.touch(exist_ok=True)
+    with open(links_path, "r") as links_file:
+        start_urls = list({link.strip() for link in links_file.readlines()})
+
+    def parse(self, response):
+        """
+        Parse the text out of the HTML returned by a given web request response.
+        Save page's text in a '.txt' file named after the article's title.
+        """
+        # HTML elements selectors
+        PARAGRAPH_SELECTOR = ".post-content > *:not([id*='comments']) > *:not([class*='sharedaddy']):not([class*='535446a9c8b556bfa11394b020286a83'])"
+        TEXT_SELECTOR = ".//text()"
+        
+        # parse
+        text = []
+        for element in response.css(PARAGRAPH_SELECTOR):
+            paragraph = ' '.join( [line.strip() for line in element.xpath(TEXT_SELECTOR).extract()])
+            if paragraph:
+                text.append(paragraph)
+
+        # Get text file name from article's title
+        title = response.css("title::text").get().strip()
+        underscore_separated = "_".join(title.split(" ")).lower()
+        file_name = re.sub('\W+', '', underscore_separated)[0:50] # arbritary limit
+        article_file = Path("../../../articles/diario_do_brasil/{0}.txt".format(file_name))
+
+        # Save text in a file
+        article_file.touch(exist_ok=True)
+        with open(article_file, "w", encoding="UTF-8") as f:
+            f.write("\n".join(text))
