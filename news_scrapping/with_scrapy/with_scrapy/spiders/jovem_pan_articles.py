898a8ce4a6e56ba8157554941290a1d713af7c51
jpvasquesc <jpvasquesc@gmail.com>
2021-10-11 16:24:27 -0300

    jovempan.com.br scrapping

---
+++ news_scrapping/with_scrapy/with_scrapy/spiders/jovem_pan_articles.py
@@ -0,0 +1,44 @@
+import scrapy
+from pathlib import Path
+import re
+
+class JovemPanArticlesSpider(scrapy.Spider):
+    """
+    Web scrapping 'spider', based on scrapy's 'Spider' class. 
+    Built to scrap the text of news articles under the 'jovempan.com.br' domain.   
+    """
+    name = 'jovem_pan_articles'
+    allowed_domains = ['jovempan.com.br']
+
+    # Get links to the news articles from which the text will be scrapped
+    links_path = Path("../../../links/jovem_pan.txt")
+    links_path.touch(exist_ok=True)
+    with open(links_path, "r") as links_file:
+        start_urls = list({link.strip() for link in links_file.readlines()})
+
+    def parse(self, response):
+        """
+        Parse the text out of the HTML returned by a given web request response.
+        Save page's text in a '.txt' file named after the article's title.
+        """
+        # HTML elements selectors
+        PARAGRAPH_SELECTOR = "div.context > *:not([class*='list-related-posts']):not([class*='post_image'])"
+        TEXT_SELECTOR = ".//text()"
+        
+        # Parse text
+        text = []
+        for element in response.css(PARAGRAPH_SELECTOR):
+            paragraph = ' '.join( [line.strip() for line in element.xpath(TEXT_SELECTOR).extract()])
+            if paragraph:
+                text.append(paragraph)
+
+        # Get text file name from article's title
+        title = response.css("title::text").get().split("|")[0].strip()
+        underscore_separated = "_".join(title.split(" ")).lower()
+        file_name = re.sub('\W+', '', underscore_separated)[0:50] # arbritary limit
+        article_file = Path("../../../articles/jovem_pan/{0}.txt".format(file_name))
+
+        # # Save text in a file
+        article_file.touch(exist_ok=True)
+        with open(article_file, "w", encoding="UTF-8") as f:
+            f.write("\n".join(text))
---
+++ news_scrapping/with_scrapy/with_scrapy/spiders/jovem_pan_links.py
@@ -0,0 +1,45 @@
+import scrapy
+from pathlib import Path
+
+class JovenPanLinksSpider(scrapy.Spider):
+    """
+    Web scrapping 'spider', based on scrapy's 'Spider' class. 
+    Built to scrap links to news articles, listed in the "http://jovempan.com.br/ultimas/' webpage.   
+    """
+    name = "jovem_pan_links"
+    allowed_domains = ["jovempan.com.br"]
+    
+    # List of pages to scrape
+    start_urls = ["http://jovempan.com.br/ultimas/"]
+    MAX_PAGES = 4
+    scrapped_pages = 0
+    
+    def parse(self, response):
+        """
+        Parse the links to news artcles out of the HTML returned by the given web request response.
+        Save all links in a '.txt' file.
+        Make a web request for the next page.
+        """
+        # HTML elements selectors
+        LINK_SELECTOR = "h2.post-title > a::attr(href)"
+        
+        # Path to .txt file
+        links_file_path = Path("../../../links/jovem_pan.txt")
+        links_file_path.touch(exist_ok=True)
+
+        # Parse and save
+        with open(links_file_path, "a") as links_file:
+            for link in response.css(LINK_SELECTOR).getall():  
+                links_file.write("{0}\n".format(link))
+
+        # Scrape the link to the next page that lists articles
+        NEXT_PAGE_SELECTOR = "ul.pagination > li > a[aria-label*='Próxima']::attr(href)"
+        next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+        
+        # Make a web request to 'next_page'
+        if next_page and (self.scrapped_pages <= self.MAX_PAGES):
+            self.scrapped_pages += 1
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+            )
