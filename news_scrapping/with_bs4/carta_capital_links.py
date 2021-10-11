6b235ea400561807d14dc3581c5ef912cb76bfe4
jpvasquesc <jpvasquesc@gmail.com>
2021-10-11 16:23:25 -0300

    Carta capital scrapping

---
+++ news_scrapping/with_bs4/carta_capital_links.py
@@ -0,0 +1,74 @@
+"""
+Get the links for news articles listed in the page "https://www.cartacapital.com.br/mais-recentes/".
+This page uses a AJAX requests for infinte scrolling.
+"""
+
+import argparse
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+def parse_input_arguments():
+    """
+    Create a command-line menu for this script using 'argparse'.
+    """
+    parser = argparse.ArgumentParser(prog="'Carta Capital' articles scrapping",
+                                     description="Scrape the webpage 'diariodobrasil.org/todas-materias/' for links of the most recent posts")
+    parser.add_argument('-p','--pages', dest='p', type=int, default=5, help="Number of pages to scrape")
+    return parser.parse_args()
+
+def get_article_listing_page(page: int):
+    """
+    Get the HTML for the article listing page
+    """
+    form_data={"next_page": str(page),
+            "max_pages": "4152",
+            "paged": str(page),
+            "pagination_type": "load-more",
+            "display_pagination": "yes",
+            "display_excerpt": "no",
+            "display_comments": "no",
+            "display_author": "yes",
+            "display_category": "yes",
+            "display_date": "yes",
+            "thumb_image_size": "full-mobile",
+            "number_of_posts": "10",
+            "parallax_effect": "no",
+            "base": "eltdf_post_layout_three",
+            "action": "readanddigest_list_ajax",}
+    page_request = requests.post("https://www.cartacapital.com.br/wp-admin/admin-ajax.php", data=form_data)
+    html = BeautifulSoup(page_request.json()["html"], features="lxml")
+    return html
+
+def get_links(html):
+    """
+    Parse links out of html and save in a '.txt' file.
+    """
+    # Create file
+    txt_path = Path("../../news_scrapping/links/carta_capital.txt")
+    txt_path.touch(exist_ok=True)
+
+    # Parse and save
+    with open(txt_path, "a") as txt_file:
+        links_list = [a_tag['href'] for a_tag in html.find_all('a', {"class": "eltdf-pt-link"}, href=True)]
+        for link in links_list:
+            txt_file.write("{0}\n".format(link))
+    
+    return len(links_list)
+
+def main():
+    """
+    Get the links for articles listed in the first 'args.p' pages from the webpage "https://www.cartacapital.com.br/mais-recentes/".
+    """
+    args = parse_input_arguments()
+    
+    num_links = 0
+    for page in range(1, args.p):
+        html = get_article_listing_page(page)
+        num_links += get_links(html)
+        print("Parsed page {0}".format(page))
+    
+    print("Done! Saved {0} links from the page 'https://www.cartacapital.com.br/mais-recentes/'.".format(num_links))
+
+if __name__ == "__main__":
+    main()
---
+++ news_scrapping/with_scrapy/with_scrapy/spiders/carta_capital_articles.py
@@ -0,0 +1,45 @@
+import scrapy
+from pathlib import Path
+import re
+
+
+class CartaCapitalArticlesSpider(scrapy.Spider):
+    """
+    Web scrapping 'spider', based on scrapy's 'Spider' class. 
+    Built to scrap the text of news articles under the 'www.cartacapital.com.br' domain.   
+    """
+    name = 'carta_capital_articles'
+    allowed_domains = ['www.cartacapital.com.br']
+
+    # Get links to the news articles from which the text will be scrapped
+    links_path = Path("../../../links/carta_capital.txt")
+    links_path.touch(exist_ok=True)
+    with open(links_path, "r") as links_file:
+        start_urls = list({link.strip() for link in links_file.readlines()})
+
+    def parse(self, response):
+        """
+        Parse the text out of the HTML returned by a given web request response.
+        Save page's text in a '.txt' file named after the article's title.
+        """
+        # HTML elements selectors
+        PARAGRAPH_SELECTOR = ".eltdf-post-text-inner > :not(div):not([class*='telegram-notice']):not([class*=leia-tbm])"
+        TEXT_SELECTOR = ".//text()"
+
+        # Parse text
+        text = []
+        for element in response.css(PARAGRAPH_SELECTOR):
+            paragraph = ' '.join( [line.strip() for line in element.xpath(TEXT_SELECTOR).extract()])
+            if paragraph:
+                text.append(paragraph)
+
+        # Get text file name from article's title
+        title = response.css("title::text").get().lower().rstrip()
+        underscore_separated = "_".join(title.split(" "))
+        file_name = re.sub('\W+', '', underscore_separated)[0:50] # arbritary limit
+        article_file = Path("../../../articles/carta_capital/{0}.txt".format(file_name))
+
+        # Save text in .txt file
+        article_file.touch(exist_ok=True)
+        with open(article_file, "w", encoding="UTF-8") as f:
+            f.write("\n".join(text))
