f45967dd9b8449f0ba95e98000d5377c3d6d6756
jpvasquesc <jpvasquesc@gmail.com>
2021-10-16 12:31:45 -0300

    Add user interfacing script

---
+++ main.py
@@ -0,0 +1,90 @@
+import argparse
+from pathlib import Path
+import glob
+import pandas as pd
+import pickle
+
+
+def main():
+    """
+    Classify input text as 'fake' or 'real' using the pre-trained machine learning model.
+    """
+    
+    # Get command-line arguemnts
+    args = parse_arguments()
+    
+    # Create a dataframe with the input texts
+    if args.file:
+        pattern_path = args.file
+        files = glob.glob(pattern_path)
+        texts = get_text_from_file(files)
+
+    else:
+        texts = pd.DataFrame(data={"text": [args.text]})
+        
+    # Predict text classification with ML model
+    predictions = classsify_input_text(texts)
+    
+    # Print predictions to user
+    predictions = predictions.drop(columns="text")
+    predictions = predictions.sort_values("prediction")
+    print(predictions)
+    
+    
+def parse_arguments():
+    """
+    Create a command-line menu for this script using 'argparse'.
+    """
+    parser = argparse.ArgumentParser(prog="Fake news detection ML model",
+                                     description="Predict if input text is 'fake' or 'real'")
+    
+    input_methods = parser.add_mutually_exclusive_group(required=True)
+    input_methods.add_argument('-t', '--text', dest='text', type=str, help="Input text from the command line")
+    input_methods.add_argument('-f','--file', dest='file', type=str, help="Input text from one or more .txt files")
+    
+    return parser.parse_args()
+
+
+def get_text_from_file(files):
+    """
+    Get the text inside each .txt file in 'files'.
+
+    Args:
+        files (list): List of .txt files
+        
+    Returns:
+        pd.Dataframe: DataFrame of texts, with filename as index
+    """
+    df = pd.DataFrame(columns={"file": [], "text": []})
+    for file in files:
+        file_path = Path(file)
+        with open(file_path, "r", encoding="UTF-8") as f:
+            row = {"file": file_path.stem, "text": f.read()}
+            df = df.append(row, ignore_index = True)
+    return df.set_index("file")
+    
+    
+def classsify_input_text(text_df):
+    """
+    Utilize the ML model to predict if the text is 'fake' or 'real'.
+
+    Args:
+        text_df (pd.Dataframe): DataFrame with input texts
+    
+    Returns:
+        pd.Dataframe: DataFrame with the prediction for each text
+    """
+    
+    # Load ML model
+    model_path = Path("./model.sav")
+    model = pickle.load(open(model_path, "rb"))
+
+    # Pass input through model
+    prediction = model.predict(text_df.text)
+    text_df["prediction"] = prediction     
+    
+    return text_df
+     
+     
+if __name__ == "__main__":
+    main()
